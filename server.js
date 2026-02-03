8cf3aa71e89b69cab57bf2c8ebbe8bb394f2b573
Abhishek <abhisheks@pearlorganisation.com>
2026-02-03 14:05:25 +0530

    'health-check'

--- server.js
+++ server.js
@@ -19,6 +19,12 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/', (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: 'WingMan API is running 🚀'
+  })
+})
 
 
 app.get('/health', (req, res) => {
