f001068ef1e6a75ead5ebf8f0e53f9f1ac762a28
Abhishek <abhisheks@pearlorganisation.com>
2026-02-03 14:49:29 +0530

    'ok'

--- utils/mail.js
+++ utils/mail.js
@@ -1,16 +1,21 @@
-const nodemailer = require('nodemailer')
-
+const nodemailer = require('nodemailer');
 
 const createMailTransporter = () => {
   return nodemailer.createTransport({
     host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
-    secure: false, // TLS
+    port: Number(process.env.SMTP_PORT), // ensure number
+    secure: false, // true only for 465
     auth: {
       user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS // Gmail App Password
-    }
-  })
-}
+      pass: process.env.SMTP_PASS
+    },
+    tls: {
+      rejectUnauthorized: false
+    },
+    connectionTimeout: 10000, // prevent hanging
+    greetingTimeout: 10000,
+    socketTimeout: 10000
+  });
+};
 
-module.exports = createMailTransporter
+module.exports = createMailTransporter;
